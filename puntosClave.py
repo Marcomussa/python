98e169765c0fbd11c57bcc33261fbed07862bbf4
Marco <marcomussa567@gmail.com>
2024-09-16 19:37:23 -0300

    l

--- puntosClave.py
+++ puntosClave.py
@@ -6,8 +6,8 @@ is_active = True
 
 #! 2. Estructura de Datos
 # Arreglos --> Mutables
-arr = [1, 2, 3, "Hello", "World"]
-print(arr[0])
+list = [1, 2, 3, "Hello", "World"]
+print(list[0])
 
 # Tuplas --> Inmutables
 tupla = (10, 20, 30)
@@ -24,7 +24,30 @@ dict = {
 }
 print(dict["name"])
 
-#! 3. Funciones 
+#! 3. Metodos
+string = "Hello World"
+string.uppper()
+string.lower()
+string.capitalize()
+string.strip() # Elimina espacios en blanco
+string.replace("Hello", "Goodbye")
+string.split(" ") # ["Hello", "World"]
+
+list.append(4)
+list.extend([4, 5])
+list.insert(2, 3) # En indice 2, inserta value 3
+list.remove(3) # Elimina la primera aparicion del parametro
+list.pop() # Elimina el ultimo. Si se le pasa parametro elimina el de dicho indice
+list.sort() # Ordena ascendente
+list.reverse() # Invierte orden
+list.index(2) # Devuelve el indice de la primera aparicion del elemento
+list.clear() # []
+
+dict.get("name")
+dict.keys()
+
+
+#! 4. Funciones 
 #* Son objetos de primera clase --> Se pueden pasar como parametros a otras funciones o asignarlas a variables 
 # Parametros
 def func(name):
@@ -53,4 +76,10 @@ def apply_func(function, value):
 
 result = apply_func(square, 2)
 
-# 
+# Anidacion
+def externa(text):
+    def interna():
+        return text.upper()
+    return interna()
+
+print(externa("sato"))
